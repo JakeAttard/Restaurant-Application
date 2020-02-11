1a4b893451fb91c26826ce4bcb5ddf932cf8b14e
Jake Attard <jakeattard18@gmail.com>
2020-02-11 17:26:11 +1000

    Migrated the Restaurant-Application-LandingPage over to Laravel. Added asset images used for the project in the public/img folder.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,95 +6,201 @@
 
         <title>Laravel</title>
 
+        <!-- Stylesheet -->
+        <link rel="stylesheet" href="css/app.css">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- Bootstrap -->
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+        <section id="welcome">
+            <div class="welcome-menu">
+                <div class="logo">
+                  <img src="https://webstockreview.net/images/clipart-restaurant-restaurant-logo-5.png" alt="Logo">
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="menu">
+                <div class="menu-title">
+                    Menu
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <ul class="links">
+                    <li>
+                    <a href="#">Menu</a>
+                    </li>
+                    <li>
+                    <a href="#">Wait List</a>
+                    </li>
+                    <li>
+                    <a href="#">Offers</a>
+                    </li>
+                    <li>
+                    <a href="#">Gift Cards</a>
+                    </li>
+                </ul>
+                </div>
+      
+                <div class="social-icons">
+                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+      
+                <div class="location">
+                <div class="address">
+                    234 Main Street,<br>
+                    Surfers Paradise, Gold Coast 4209
+                </div>
+                <div class="phone-number">
+                    <a href="tel:+61452525251">0452-525-251</a>
+                </div>
                 </div>
             </div>
-        </div>
+      
+            <div class="welcome-jumbo">
+              <div class="status">NEW</div>
+              <h1>Billy Burger</h1>
+              <img src="img/billyBurgerImg.png" class="burger-fries" alt="">
+            </div>
+            </section>
+      
+            <section id="information">
+              <div class="restaurant-image">
+                <img src="img/top-down.jpg" alt="">
+              </div>
+      
+              <div class="info">
+                <h2>We Started From The Bottom in 1987</h2>
+                <div class="paragraphs">
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam sequi aliquid fugiat reprehenderit incidunt veniam esse. Iure nisi obcaecati officiis ab assumenda dolorem amet libero. Expedita ut odit beatae rem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam sequi aliquid fugiat reprehenderit incidunt veniam esse. Iure nisi obcaecati officiis ab assumenda dolorem amet libero. Expedita ut odit beatae rem.</p>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam sequi aliquid fugiat reprehenderit incidunt veniam esse. Iure nisi obcaecati officiis ab assumenda dolorem amet libero. Expedita ut odit beatae rem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam sequi aliquid fugiat reprehenderit incidunt veniam esse. Iure nisi obcaecati officiis ab assumenda dolorem amet libero. Expedita ut odit beatae rem.</p>
+                </div>
+      
+                <a href="#" class="about-link">
+                  <span>Learn more about restaurants</span>
+                  <div class="circle-right">
+                    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                  </div>
+                </a>
+              </div>
+            </section>
+      
+            <section id="food-preview">
+              <h2>We have everything you need to kill your hunger</h2>
+              <div class="button-rounded">
+                View Our Menu
+              </div>
+              <div class="container">
+                <div class="left-btn">
+                  <i class="fa fa-chevron-left" aria-hidden="true"></i>
+                </div>
+                <div class="right-btn">
+                  <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                </div>
+                <div class="food-slider">
+                  <div class="sliding-system">
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Starters
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="img/startersImg.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Burgers
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="img/burgerImg.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Entrees
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="img/entreeImg.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Sides
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="img/sideImg.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Deserts
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="img/desertImg.png" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Drinks
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="https://i.dlpng.com/static/png/3877536-ice-cream-desserts-transparent-background-png-png-arts-png-desserts-460_244_preview.webp" alt="">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="slide">
+                      <div class="background"></div>
+                      <div class="content">
+                        <div class="food-title">
+                          Beers
+                        </div>
+                        <p class="food-description">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!
+                        </p>
+                        <div class="food-image">
+                          <img src="https://i.dlpng.com/static/png/3877536-ice-cream-desserts-transparent-background-png-png-arts-png-desserts-460_244_preview.webp" alt="">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
     </body>
 </html>

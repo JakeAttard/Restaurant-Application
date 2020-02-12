29af38d68f762a0f4ad00fbe03c36df267ed9074
Jake Attard <jakeattard18@gmail.com>
2020-02-12 17:05:23 +1000

    Setting up basic main template layouts for the applciation.

---
+++ resources/views/includes/side-menu.blade.php
@@ -0,0 +1,42 @@
+<div class="welcome-menu">
+    <div class="logo">
+        <a href="/">
+            <img src="https://webstockreview.net/images/clipart-restaurant-restaurant-logo-5.png" alt="Logo">
+        </a>
+    </div>
+    <div class="menu">
+    <div class="menu-title">
+        Menu
+    </div>
+    <ul class="links">
+        <li>
+        <a href="/menu">Menu</a>
+        </li>
+        <li>
+        <a href="/waitlist">Wait List</a>
+        </li>
+        <li>
+        <a href="/offers">Offers</a>
+        </li>
+        <li>
+        <a href="/giftcards">Gift Cards</a>
+        </li>
+    </ul>
+    </div>
+
+    <div class="social-icons">
+    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </div>
+
+    <div class="location">
+    <div class="address">
+        234 Main Street,<br>
+        Surfers Paradise, Gold Coast 4209
+    </div>
+    <div class="phone-number">
+        <a href="tel:+61452525251">0452-525-251</a>
+    </div>
+    </div>
+</div>

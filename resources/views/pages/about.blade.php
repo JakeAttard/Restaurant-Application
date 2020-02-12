9f28d16311d30d2162b472b3e632e57b656a83e1
Jake Attard <jakeattard18@gmail.com>
2020-02-12 23:49:44 +1000

    About page created and styled.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbo">
-        <h1>About Page</h1>
+  <div id="about-page">
+    <div class="content-box">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>About Page</h1>
+          <p>Lorem ipsum dolor amet vice pop-up hammock mustache pour-over. Helvetica deep v whatever raclette copper mug. Keffiyeh blog bushwick pop-up church-key. Banh mi bicycle rights keffiyeh vice adaptogen photo booth leggings air plant flexitarian craft beer lyft lumbersexual jianbing sriracha. Lyft salvia hella live-edge knausgaard actually. Mixtape photo booth air plant, artisan tattooed schlitz chillwave bitters echo park direct trade palo santo plaid retro kickstarter. Organic taiyaki flexitarian meh.</p>
+          <p>Lorem ipsum dolor amet vice pop-up hammock mustache pour-over. Helvetica deep v whatever raclette copper mug. Keffiyeh blog bushwick pop-up church-key. Banh mi bicycle rights keffiyeh vice adaptogen photo booth leggings air plant flexitarian craft beer lyft lumbersexual jianbing sriracha. Lyft salvia hella live-edge knausgaard actually. Mixtape photo booth air plant, artisan tattooed schlitz chillwave bitters echo park direct trade palo santo plaid retro kickstarter. Organic taiyaki flexitarian meh.</p>
+        </div>
+        <div class="col-md-6">
+          <img src="img/about-us-img.jpeg" class="img-fluid">
+        </div>
+      </div> 
+    </div>
   </div>
 @endsection

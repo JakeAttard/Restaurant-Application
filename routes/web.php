42f6c6dbf41453679a1f8a69b85620e1c5a993b7
Jake Attard <jakeattard18@gmail.com>
2020-02-19 17:02:44 +1000

    Created admin users page.

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,11 @@ Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
 Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
 Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 
+// Admin Users
+Route::get('/admin/users', 'admin\UsersController@index');
+Route::get('/admin/users/create', 'admin\UsersController@create');
+Route::get('/admin/users/{id}/edit', 'admin\UsersController@edit');
+
 // Admin Authentication
 Route::get('/admin/register', function () {
     return view('admin/register');

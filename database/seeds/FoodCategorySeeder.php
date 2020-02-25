c64df9a220ad7c45432466431997ff87a76bce61
Jake Attard <jakeattard18@gmail.com>
2020-02-25 20:57:55 +1000

    Created seeds for the food categories and items.

--- database/seeds/FoodCategorySeeder.php
+++ database/seeds/FoodCategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class FoodCategorySeeder extends Seeder
 {
@@ -11,6 +12,44 @@ class FoodCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('food_categories')->insert([
+            'title' => 'Starters',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/startersImg.png',
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_categories')->insert([
+            'title' => 'Burgers',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/burgerImg.png',
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_categories')->insert([
+            'title' => 'Entrees',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/entreeImg.png',
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_categories')->insert([
+            'title' => 'Sides',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/sideImg.png',
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_categories')->insert([
+            'title' => 'Deserts',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/desertImg.png',
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
     }
 }
--- database/seeds/FoodItemSeeder.php
+++ database/seeds/FoodItemSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class FoodItemSeeder extends Seeder
 {
@@ -11,6 +12,34 @@ class FoodItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('food_items')->insert([
+            'title' => 'Texas Burger',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/startersImg.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_items')->insert([
+            'title' => 'BBQ Burger',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/startersImg.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
+
+        DB::table('food_items')->insert([
+            'title' => 'Jakes Burger',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum numquam, iusto iure debitis, porro ipsa temporibus voluptates qui praesentium obcaecati impedit, sequi quod quos fuga? Voluptatum ex animi error totam!',
+            'image_url' => 'img/startersImg.png',
+            'price' => 9.99,
+            'category_id' => 2,
+            'updated_at' => Carbon::now(),
+            'created_at' => Carbon::now()
+        ]);
     }
 }

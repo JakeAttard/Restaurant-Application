f1d35bc273a863795d22d929fb1df6f200b02c1d
Jake Attard <jakeattard18@gmail.com>
2020-02-27 20:24:27 +1000

    Create members model controller, migrations and seeder.

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,10 @@ Route::put('/admin/food-items/{id}', 'admin\FoodItemsController@update');
 Route::delete('/admin/food-items/{id}/delete', 'admin\FoodItemsController@delete');
 
 // Admin Customers
-Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
+// Admin Members
+Route::get('/admin/members', 'admin\MemberController@index');
+
+// Admin Reservations
 Route::get('/admin/reservations', 'admin\CustomersController@allReservations');
 
 // Admin Users

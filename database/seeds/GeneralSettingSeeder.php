00cc2ba23a5b8ab9ee52b84dc7286b2d788fcbf6
Jake Attard <jakeattard18@gmail.com>
2020-03-07 17:18:07 +1000

    Created the contact page and embedded google maps showing the location.

--- database/seeds/GeneralSettingSeeder.php
+++ database/seeds/GeneralSettingSeeder.php
@@ -13,14 +13,14 @@ class GeneralSettingSeeder extends Seeder
     public function run()
     {
         DB::table('general_settings')->insert([
-            'site_title' => 'Jakes Burgers',
+            'site_title' => 'Hungry Jacks',
             'logo_image_url' => 'https://webstockreview.net/images/clipart-restaurant-restaurant-logo-5.png',
-            'address_1' => '234 Main Street',
+            'address_1' => '35 Old Coach Road',
             'address_2' => '',
-            'city' => 'New York',
-            'state' => 'NY',
-            'zipcode' => '11385',
-            'phone_number' => '341-231-4545',
+            'city' => 'Upper Coomera',
+            'state' => 'Queensland',
+            'zipcode' => '4209',
+            'phone_number' => '(07) 5580 1526',
             'updated_at' => Carbon::now(),
             'created_at' => Carbon::now()
         ]);

e07eac6bd1e401c9618392b57aa95254c6450cbe
Jake Attard <jakeattard18@gmail.com>
2020-03-21 00:34:32 +1000

    Bug fixes with the menu slider.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,20 +23,20 @@
             let moveTotal;
             let currentSlide = 0;
 
-            $(window).resize(function() {
+            $(window).resize(function(){
                 currentSlide = 0;
-                $('.sliding-system').css('transform', 'translate3d(-0%, 0, 0)');
+                $('.sliding-system').css('transform', 'translate3d(-0%, 0,0');
             })
 
-            function desktopSlider() {
-                if(a.matches) {
+            function desktopSlider(){
+                if(a.matches){
                     maxSlides = Math.ceil((slides * 20) / 100);
                     moveTotal = 100;
 
                     // Right Button
                     $('.right-btn').on('click', () => {
-                        console.log('clicked right btn');
-                        if(currentSlide < maxSlides) {
+                        console.log('clicked right btn')
+                        if(currentSlide < maxSlides){
                             currentSlide += 100;
                             $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
                         }
@@ -44,8 +44,8 @@
 
                     // Left Button
                     $('.left-btn').on('click', () => {
-                        console.log('clicked left btn');
-                        if(currentSlide != 0) {
+                        console.log('clicked right btn')
+                        if(currentSlide != 0){
                             currentSlide -= 100;
                             $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
                         }
@@ -53,41 +53,38 @@
                 }
             }
 
-            var a = window.matchMedia("(min-wdith: 998px)");
-            console.log(a);
+            var a = window.matchMedia("(min-width: 998px)")
             desktopSlider(a);
             a.addListener(desktopSlider);
 
-            function tabletSlider() {
-                if(b.matches) {
+            function tabletSlider(){
+                if(b.matches){
                     maxSlides = Math.ceil((slides * 50) / 100);
                     moveTotal = 100;
-                    
+
                     // Right Button
                     $('.right-btn').on('click', () => {
-                        console.log('clicked right btn');
-                        if(currentSlide < ((maxSlides * 100) -100)) {
+                        console.log('clicked right btn')
+                        if(currentSlide < ((maxSlides * 100) - 100)){
                             currentSlide += 50;
                             $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
                         }
                     });
-
                     // Left Button
                     $('.left-btn').on('click', () => {
-                        console.log('clicked left btn');
-                        if(currentSlide != 0) {
+                        console.log('clicked right btn')
+                        if(currentSlide != 0){
                             currentSlide -= 50;
                             $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
                         }
                     });
                 }
             }
-
-            var b = window.matchMedia("(min-wdith: 768px) and (max-width: 997px)");
-            console.log(b);
+            
+            var b = window.matchMedia("(min-width: 768px) and (max-width: 997px");
+            console.log(b)
             tabletSlider(b);
             b.addListener(tabletSlider);
-
         </script>
     </body>
 </html>
